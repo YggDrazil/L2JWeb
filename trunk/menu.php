62598c8bc5333c0fea834dca29b1f9ab80f72985
sebastien.gascon <sebastien.gascon@3df7fe5c-7aca-64f6-1bcd-b1366662e614>
2010-07-22 14:24:18 +0000

    1st commit

---
+++ trunk/menu.php
@@ -0,0 +1,49 @@
+<?php
+/*************************************************/
+/* Project Name.: L2J-Web				*/
+/* File Name....: menu.php				*/
+/* Author.......: Sebastien Gascon			*/
+/* Author Email.: sebastien.gascon@gmail.com	*/
+/* Last Updated.: 2007-01-25 09:09			*/
+/* Created On...: 25/01/2007 9:09:42 AM		*/
+/* Last Updated.: 22/07/2010 10:06:40 AM		*/
+/*************************************************/
+echo "<table width=\"100%\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" background=\"images/header/hmm.jpg\">";
+echo "<tr>";
+echo "<td width=\"361\" height=\"224\" valign=\"top\" colspan=\"5\"><img src=\"images/header/A.jpg\" width=\"361\" height=\"224\"></td>";
+echo "<td width=\"100%\" align=\"center\" valign=\"top\"><img src=\"header_status.php\" width=\"107\" height=\"224\"></td>";
+echo "<td width=\"361\" height=\"224\" valign=\"top\" colspan=\"2\"><img src=\"images/header/C.jpg\" width=\"361\" height=\"224\"></td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td width=\"43\" height=\"33\" valign=\"top\"><img src=\"images/header/D.jpg\" width=\"43\" height=\"33\"></td>";
+echo "<td width=\"82\" height=\"33\" valign=\"top\"><a href=\"index.php\"><img src=\"images/header/E.jpg\" width=\"82\" height=\"33\" border=\"0\"></a></td>";
+echo "<td width=\"75\" height=\"33\" valign=\"top\"><img src=\"images/header/F.jpg\" width=\"75\" height=\"33\"></td>";
+echo "<td width=\"75\" height=\"33\" valign=\"top\"><img src=\"images/header/G.jpg\" width=\"75\" height=\"33\"></td>";
+echo "<td width=\"87\" height=\"33\" valign=\"top\"><img src=\"images/header/H.jpg\" width=\"87\" height=\"33\"></td>";
+echo "<td width=\"100%\" align=\"center\" valign=\"top\"><img src=\"images/header/I.jpg\" width=\"107\" height=\"33\"></td>";
+echo "<td width=\"213\" height=\"33\" valign=\"top\"><img src=\"images/header/J.jpg\" width=\"213\" height=\"33\"></td>";
+echo "<td width=\"149\" height=\"33\" valign=\"top\"><img src=\"images/header/K.jpg\" width=\"149\" height=\"33\"></td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td width=\"361\" height=\"43\" valign=\"top\" colspan=\"5\"><img src=\"images/header/L.jpg\" width=\"361\" height=\"43\"></td>";
+echo "<td width=\"100%\" align=\"center\" valign=\"top\"><img src=\"images/header/M.jpg\" width=\"107\" height=\"43\"></td>";
+echo "<td width=\"361\" height=\"43\" valign=\"top\" colspan=\"2\"><img src=\"images/header/N.jpg\" width=\"361\" height=\"43\"></td>";
+echo "</tr>";
+echo "</table>";
+
+
+//echo "<img src=\"http://www.wydgaming.com/blocks/gameservers_image/l2.php\"><br/>";
+echo "<a href=\"index.php\">Home</a> - ";
+echo "<a href=\"online.php\">Online</a> - ";
+echo "<a href=\"players.php\">Players</a> - ";
+//echo "<a href=\"memcard.php\">My Account</a> - ";
+echo "<a href=\"spellbooks.php\">Spellbooks</a> - ";
+echo "<a href=\"monsters.php\">Mobs</a> - ";
+echo "<a href=\"armors.php\">Armor</a> - ";
+echo "<a href=\"weapons.php\">Weapons</a> - ";
+echo "<a href=\"items.php\">Items</a> - ";
+echo "<a href=\"map.php\">Map</a> - ";
+echo "<a href=\"logout.php\">Logout</a> - ";
+echo "<br/>";
+
+?>
